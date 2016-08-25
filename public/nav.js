e8935986db7ea00fd140a65c282c3b16f6733b22
Tom Bradley <bradth01@gmail.com>
2016-08-24 22:11:33 -0400

    nav p much done

--- public/nav.js
+++ public/nav.js
@@ -1,16 +1,20 @@
 'use strict';
 
 $(function() {
-// nav span scrolling functionality
+    
+    // nav span scrolling functionality
     $('.page-link').click(function() {
         $('html, body').animate({
             scrollTop: $($(this).attr('rel')).offset().top -75
         }, 700);
-// prevent element id from appearing in url
+        // add active class when clicked, making sure only one is active
+        $('.page-link').removeClass('active');
+        $($(this).addClass('active'));
+        // prevent element id from appearing in url
         event.preventDefault();
     });
 
-// change menu item color when hovered over
+    // change menu item color when hovered over
     $('.page-link').hover(function() {
         $(this).css('color', 'black');
     }, function() {
